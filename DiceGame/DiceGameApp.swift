53c880c77ef291b5c2101f2328cc303ad8dc632d
Ian Slane <slaneian@gmail.com>
2023-04-12 17:58:19 -0600

    Initial Commit

---
+++ DiceGame/DiceGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceGameApp.swift
+//  DiceGame
+//
+//  Created by Ian Slane on 4/12/23.
+//
+
+import SwiftUI
+
+@main
+struct DiceGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
